3daa621c7075255f02cb6cf763a8416a8ff52b2e
Lucas Nabeto <lucas.nabeto@outlook.com>
2025-12-28 20:11:33 -0300

    fix: update scope name for cognito and add https ingress for alb

--- elb/main.tf
+++ elb/main.tf
@@ -31,10 +31,20 @@ resource "aws_security_group" "alb_sg" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "alb_http_in" {
+  description = "HTTP from internet"
   security_group_id = aws_security_group.alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_https_in" {
+  description = "HTTPS from internet"
+  security_group_id = aws_security_group.alb_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
   ip_protocol       = "tcp"
 }
 
