77951386ef30952b80ed3e85e4d41d9f4586f089
Lucas Nabeto <lucas.nabeto@outlook.com>
2026-01-19 20:55:30 -0300

    chore: change S3 bucket name in Terraform configuration

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "baitersburger-products-infra"
+    bucket = "baitersburger-products-infrastructure"
     key    = "terraform/infra/state.tfstate"
     region = "us-east-1"
   }
